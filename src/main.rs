79bd6add385db617bb01fed5def5e1ae86420210
Ziyi Yan <ziyi.yan@foxmail.com>
2020-07-12 20:57:51 +0800

    finish 200-line implementation
    Signed-off-by: Ziyi Yan <ziyi.yan@foxmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,224 @@
 //! https://cfsamson.gitbook.io/green-threads-explained-in-200-lines-of-rust/green-threads.
 #![deny(missing_docs)]
 #![feature(llvm_asm)]
+#![feature(naked_functions)]
 
-/// SSIZE is a small stack size in bytes for easy debugging.
-const SSIZE: isize = 48;
+use std::ptr;
 
-/// #[repr(C)] tells compiler to use a stable ABI in C way for struct ThreadContext.
+const DEFAULT_STACK_SIZE: usize = 1024 * 1024 * 2;
+const MAX_THREADS: usize = 4;
+static mut RUNTIME: usize = 0;
+
+/// Runtime schedule and switch threads. current is the id of thread which is currently running.
+pub struct Runtime {
+    threads: Vec<Thread>,
+    current: usize,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+enum State {
+    // available and ready to be assigned a task if needed
+    Available,
+    // running
+    Running,
+    // ready to move forward and resume execution
+    Ready,
+}
+
+/// ThreadContext contains the registers marked as "callee-saved" (preserved across calls)
+/// in the specification of x86-64 architecture. They contain all the information
+/// we need to resume a thread.
 #[derive(Debug, Default)]
 #[repr(C)]
 struct ThreadContext {
-    rsp: u64, // the stack pointer
+    rsp: u64,
+    r15: u64,
+    r14: u64,
+    r13: u64,
+    r12: u64,
+    rbx: u64,
+    rbp: u64,
 }
 
-fn hello() -> ! {
-    println!("I LOVE WAKING UP ON A NEW STACK!");
-
-    loop {}
+struct Thread {
+    id: usize,
+    stack: Vec<u8>,
+    ctx: ThreadContext,
+    state: State,
 }
 
-/// gt_switch switch over to new stack.
-///
-/// Register %rsp description from page 33 in https://github.com/hjl-tools/x86-psABI/wiki/x86-64-psABI-1.0.pdf:
-///     The stack pointer holds the address of the byte with lowest address which is part of the stack.
-///     It is guaranteed to be 16-byte aligned at process entry.
-unsafe fn gt_switch(new: *const ThreadContext) {
-    llvm_asm!(
-        "
-        mov 0x00($0), %rsp
+impl Thread {
+    fn new(id: usize) -> Self {
+        Thread {
+            id,
+            stack: vec![0_u8; DEFAULT_STACK_SIZE],
+            ctx: ThreadContext::default(),
+            state: State::Available,
+        }
+    }
+
+    fn new_with_state(id: usize, state: State) -> Self {
+        Thread {
+            id,
+            stack: vec![0_u8; DEFAULT_STACK_SIZE],
+            ctx: ThreadContext::default(),
+            state,
+        }
+    }
+}
+
+impl Runtime {
+    /// Initialize with a base thread.
+    pub fn new() -> Self {
+        let base_thread_id = 0;
+        let base_thread = Thread::new_with_state(base_thread_id, State::Running);
+
+        let mut threads = vec![base_thread];
+        let mut available_threads = (1..MAX_THREADS).map(|i| Thread::new(i)).collect();
+        threads.append(&mut available_threads);
+
+        Runtime {
+            threads,
+            current: base_thread_id,
+        }
+    }
+
+    /// This is cheating a bit, but we need a pointer to our Runtime
+    /// stored so we can call yield on it even if we don't have a
+    /// reference to it.
+    pub fn init(&self) {
+        unsafe {
+            let r_ptr: *const Runtime = self;
+            RUNTIME = r_ptr as usize;
+        }
+    }
+
+    /// start the runtime
+    pub fn run(&mut self) -> ! {
+        while self.t_yield() {}
+        std::process::exit(0);
+    }
+
+    fn t_return(&mut self) {
+        if self.current != 0 {
+            self.threads[self.current].state = State::Available;
+            self.t_yield();
+        }
+    }
+
+    fn t_yield(&mut self) -> bool {
+        let mut pos = self.current;
+        while self.threads[pos].state != State::Ready {
+            pos += 1;
+            if pos == self.threads.len() {
+                pos = 0;
+            }
+            if pos == self.current {
+                return false;
+            }
+        }
+
+        if self.threads[self.current].state != State::Available {
+            self.threads[self.current].state = State::Ready;
+        }
+
+        self.threads[pos].state = State::Running;
+        let old_pos = self.current;
+        self.current = pos;
+
+        unsafe {
+            switch(&mut self.threads[old_pos].ctx, &self.threads[pos].ctx);
+        }
+        // Prevents compiler from optimizing our code away on Windows.
+        self.threads.len() > 0
+    }
+
+    /// spawn a function to be executed by runtime
+    pub fn spawn(&mut self, f: fn()) {
+        let available = self
+            .threads
+            .iter_mut()
+            .find(|t| t.state == State::Available)
+            .expect("no available thread.");
+
+        let size = available.stack.len();
+        let s_ptr = available.stack.as_mut_ptr();
+
+        unsafe {
+            // put the f to the 16 bytes aligned position.
+            ptr::write(s_ptr.offset((size - 32) as isize) as *mut u64, f as u64);
+            // put the guard 1 byte next to the f for being executed after f returned.
+            ptr::write(s_ptr.offset((size - 24) as isize) as *mut u64, guard as u64);
+
+            available.ctx.rsp = s_ptr.offset((size - 32) as isize) as u64;
+        }
+        available.state = State::Ready;
+    }
+}
+
+fn guard() {
+    unsafe {
+        let rt_ptr = RUNTIME as *mut Runtime;
+        (*rt_ptr).t_return();
+    }
+}
+
+/// yield_thread is a helper function that lets us call yield from an arbitrary place in our code.
+pub fn yield_thread() {
+    unsafe {
+        let rt_ptr = RUNTIME as *mut Runtime;
+        (*rt_ptr).t_yield();
+    };
+}
+
+#[naked]
+#[inline(never)]
+unsafe fn switch(old: *mut ThreadContext, new: *const ThreadContext) {
+    llvm_asm!("
+        mov     %rsp, 0x00($0)
+        mov     %r15, 0x08($0)
+        mov     %r14, 0x10($0)
+        mov     %r13, 0x18($0)
+        mov     %r12, 0x20($0)
+        mov     %rbx, 0x28($0)
+        mov     %rbp, 0x30($0)
+
+        mov     0x00($1), %rsp
+        mov     0x08($1), %r15
+        mov     0x10($1), %r14
+        mov     0x18($1), %r13
+        mov     0x20($1), %r12
+        mov     0x28($1), %rbx
+        mov     0x30($1), %rbp
         ret
         "
-        :
-        : "r"(new)
-        :
-        : "alignstack" // it woll work without this now, will need it later
+    :
+    :"r"(old), "r"(new)
+    :
+    : "volatile", "alignstack"
     );
 }
 
 fn main() {
-    let mut ctx = ThreadContext::default();
-    let mut stack = vec![0_u8; SSIZE as usize];
-    unsafe {
-        let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
-        println!("stack_bottom: {:?}", stack_bottom as usize);
-        let sb_aligned = (stack_bottom as usize & !15) as *mut u8; // align backwards to 16 bytes
-        println!("sb_aligned: {:?}", sb_aligned as usize);
-        std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64); // another -16 bytes offset for alignment reason
-        ctx.rsp = sb_aligned.offset(-16) as u64;
-
-        // print the stack,
-        // remember the stack starts on the top and the address should align to the top in 16 bytes.
-        print_stack(stack.as_ptr());
-
-        gt_switch(&mut ctx);
-    }
-}
-
-// print stack from the top to bottom.
-unsafe fn print_stack(stack_ptr: *const u8) {
-    for i in (0..SSIZE).rev() {
-        println!(
-            "{}: mem: {}, val: {}",
-            i,
-            stack_ptr.offset(i) as usize,
-            *stack_ptr.offset(i)
-        );
-    }
+    let mut runtime = Runtime::new();
+    runtime.init();
+    runtime.spawn(|| {
+        println!("THREAD 1 STARTING");
+        let id = 1;
+        for i in 0..10 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 1 FINISHED");
+    });
+    runtime.spawn(|| {
+        println!("THREAD 2 STARTING");
+        let id = 2;
+        for i in 0..15 {
+            println!("thread: {} counter: {}", id, i);
+            yield_thread();
+        }
+        println!("THREAD 2 FINISHED");
+    });
+    runtime.run();
 }
